0c1bab1e2603259e0187a755c6c6d82b47bc6a16
Priyansh Patel <priyansh.180804@gmail.com>
2024-02-01 13:25:24 +0530

    Session Not Setting

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,15 @@
 // Basic Imports
-import MongoDBStore from "connect-mongodb-session";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import "dotenv/config";
 import express, { Express } from "express";
-import session from "express-session";
 import mongoose from "mongoose";
 import path from "path";
 // Router Imports
 import userRouter from "./routes/userRouter";
+// Session Management
+import MongoDBStore from "connect-mongodb-session";
+import session from "express-session";
 
 // Creating Backend Application
 const app: Express = express();
@@ -17,11 +18,12 @@ const app: Express = express();
 const MongoStore = MongoDBStore(session);
 
 // Creating a new MongoDBStore
-const store = new MongoStore({
+const store: MongoDBStore.MongoDBStore = new MongoStore({
   uri: process.env.DB_URL!,
   collection: "sessions",
 });
 
+// Middlewares
 app.use(
   session({
     secret: process.env.SECRET_KEY!,
@@ -35,7 +37,7 @@ app.use(
     },
   })
 );
-// Middlewares
+// Basic Middlewares
 app.use(
   cors({
     origin: "https://advanced-login-app.vercel.app",
--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -5,20 +5,16 @@ import allowOnlyLoggedInUser from "../middlewares/userAuth";
 const userRouter: Router = Router();
 
 userRouter
-  .get("/", allowOnlyLoggedInUser, userController.getUser)
-  .get("/getEmail", userController.getEmail)
-  .post("/login", userController.loginUser)
-  .post(
-    "/register",
-    upload.single("profilePicture"),
-    userController.registerUser
-  )
-  .post("/logout", userController.logoutUser)
-  .put("/update", upload.single("profilePicture"), userController.updateUser)
-  .post("/sendMail", userController.sendMail)
-  .post("/verifyOtp", userController.verifyOtp)
-  .post("/resetPassword", userController.resetPassword)
-  .post("/googleRegister", userController.googleRegister)
-  .post("/googleLogin", userController.googleLogin);
+.get("/", allowOnlyLoggedInUser ,userController.getUser)
+.get("/getEmail", userController.getEmail)
+.post("/login", userController.loginUser)
+.post("/register",upload.single("profilePicture"),userController.registerUser)
+.post("/logout", userController.logoutUser)
+.put("/update", upload.single("profilePicture"), userController.updateUser)
+.post("/sendMail", userController.sendMail)
+.post("/verifyOtp", userController.verifyOtp)
+.post("/resetPassword", userController.resetPassword)
+.post("/googleRegister", userController.googleRegister)
+.post("/googleLogin", userController.googleLogin)
 
 export default userRouter;
